f8859a2a0c41b630b012a3ca334ac1fafb10fc75
Elena <edleeenna@hotmail.com>
2016-09-07 19:57:05 +1000

    Fixed ship placement on deployment

--- src/DeploymentController.cs
+++ src/DeploymentController.cs
@@ -70,7 +70,7 @@ namespace BattleShips
 				if (GameController.HumanPlayer.ReadyToDeploy & UtilityFunctions.IsMouseInRectangle(PLAY_BUTTON_LEFT, TOP_BUTTONS_TOP, PLAY_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT)) {
 					GameController.EndDeployment();
 				} else if (UtilityFunctions.IsMouseInRectangle(UP_DOWN_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT)) {
-					_currentDirection = Direction.LeftRight;
+					_currentDirection = Direction.UpDown;
 				} else if (UtilityFunctions.IsMouseInRectangle(LEFT_RIGHT_BUTTON_LEFT, TOP_BUTTONS_TOP, DIR_BUTTONS_WIDTH, TOP_BUTTONS_HEIGHT)) {
 					_currentDirection = Direction.LeftRight;
 				} else if (UtilityFunctions.IsMouseInRectangle(RANDOM_BUTTON_LEFT, TOP_BUTTONS_TOP, RANDOM_BUTTON_WIDTH, TOP_BUTTONS_HEIGHT)) {
@@ -96,8 +96,9 @@ namespace BattleShips
 			//Calculate the row/col clicked
 			int row = 0;
 			int col = 0;
-			row = Convert.ToInt32(Math.Floor((mouse.Y) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
+			row = Convert.ToInt32(Math.Floor((mouse.Y- UtilityFunctions.FIELD_TOP) / (UtilityFunctions.CELL_HEIGHT + UtilityFunctions.CELL_GAP)));
 			col = Convert.ToInt32(Math.Floor((mouse.X - UtilityFunctions.FIELD_LEFT) / (UtilityFunctions.CELL_WIDTH + UtilityFunctions.CELL_GAP)));
+
 
 			if (row >= 0 & row < GameController.HumanPlayer.PlayerGrid.Height) {
 				if (col >= 0 & col < GameController.HumanPlayer.PlayerGrid.Width) {
@@ -134,7 +135,7 @@ namespace BattleShips
 			//DrawShips
 			foreach (ShipName sn in Enum.GetValues(typeof(ShipName))) {
 				int i = 0;
-				i = Convert.ToInt32(sn) - 1;
+				 i = ((int)sn) - 1;
 				if (i >= 0) {
 					if (sn == _selectedShip) {
 						SwinGame.DrawBitmap(GameResources.GameImage("SelectedShip"), SHIPS_LEFT, SHIPS_TOP + i * SHIPS_HEIGHT);
@@ -168,7 +169,7 @@ namespace BattleShips
 		{
 			foreach (ShipName sn in Enum.GetValues(typeof(ShipName))) {
 				int i = 0;
-				i = Convert.ToInt32(sn) - 1;
+				i = ((int)sn) - 1;
 
 				if (UtilityFunctions.IsMouseInRectangle(SHIPS_LEFT, SHIPS_TOP + i * SHIPS_HEIGHT, SHIPS_WIDTH, SHIPS_HEIGHT)) {
 					return sn;
